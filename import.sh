97825327a295ae2995d5b780f7f3a6731dacfc6d
John Vasileff <john@vasileff.com>
2015-01-29 18:17:36 -0500

    better name for fat jar

--- import.sh
+++ import.sh
@@ -3,6 +3,6 @@
 ceylon import-jar \
   --out=./modules-deps \
   --force \
-  --descriptor=fat.jar.deps.properties \
-  fat.jar.deps/1.0.0 \
-  build/libs/sandbox-ceylon-snap-all.jar
+  --descriptor=csmpetstoredeps.properties \
+  com.vasileff.csmpetstoredeps/1.0.0 \
+  build/libs/csm-petstore-all.jar
--- source/com/vasileff/csmpetstore/module.ceylon
+++ source/com/vasileff/csmpetstore/module.ceylon
@@ -1,5 +1,5 @@
 module com.vasileff.csmpetstore "0.0.1" {
-    import fat.jar.deps "1.0.0";
+    import com.vasileff.csmpetstoredeps "1.0.0";
     import ceylon.collection "1.1.1";
     import ceylon.dbc "1.1.1";
     import ceylon.interop.java "1.1.1";
